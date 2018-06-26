4f09fe945fdc1af7d73f94212167d9a8fc3212d8
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-06-26 16:52:28 +0000

    automatically backed up by learn

--- models/class.rb
+++ models/class.rb
@@ -20,3 +20,26 @@ class Dog
   end
   
 end
+
+class Dog 
+  
+  @@all_names = []
+  @@count = 0
+  attr_accessor: name, age
+  
+  def initialize(name, age)
+    @name = name
+    @age = name
+    @@all_name << name
+    @@count += 1
+  end
+  
+  def self.all_names
+    @@all_names
+  end
+  
+  def self.count
+    @@count
+  end
+  
+end
